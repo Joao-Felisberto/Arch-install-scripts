ccb80a1cab46d588ba11c02b3cec51d45e301ef7
Joao-Felisberto <joaofelisberto@ua.pt>
2020-11-11 11:57:20 +0000

    my dumbness is not measurable by human means therefore I needed to make this commit

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 #	1	2	3	4
 #$root_pass $user $user_pass $hostname
 
-ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Lisbon /etc/localtime
 hwclock --systohc
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -22,9 +22,9 @@ echo "KEYMAP=pt-latin1" >> /etc/vconsole.conf
 touch /etc/hostname
 echo $4 >> /etc/hostname
 
-cat "127.0.0.1	localhost" >> /etc/hosts
-cat "::1		localhost" >> /etc/hosts
-cat "127.0.1.1	$4.localdomain	$4" >> /etc/hosts
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1	$4.localdomain	$4" >> /etc/hosts
 
 echo $1\n$1| passwd
 

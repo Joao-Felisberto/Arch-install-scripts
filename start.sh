72d8bb9f7dc32e68076a3a1eaa960722fb6da5c9
Joao-Felisberto <joaofelisberto@ua.pt>
2020-11-12 18:04:11 +0000

    Figured how these variable thingies work and swapped the order of the mount parameters

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 declare -a cfg
 while read line
 do
@@ -7,18 +9,20 @@ done < "packages.install"
 read -p "boot partition name: " boot_part
 read -p "root partition name: " root_part
 read -sp "root password: " root_pass
+echo " "
 read -p "user name: " user
-read -sp '$user password: ' user_pass
+read -sp "password for $user: " user_pass
+echo " "
 read -p "hostname: " hostname
 
 timedatectl set-ntp true
 
-mkfs.ext4 '$root_part'
-mkfs.vfat -F 32 '$boot_part'
-mount /mnt '$root_part'
+mkfs.ext4 "$root_part"
+mkfs.vfat -F 32 "$boot_part"
+mount "$root_part" /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/EFI
-mount /mnt/boot/EFI '$boot_part'
+mount "$boot_part" /mnt/boot/EFI
 
 pacstrap /mnt ${pacstrap_pkgs[*]} # installs all required packages
 
